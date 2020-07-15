209feaeec5be7703748ad4ed858db52db416b802
Norrismi <Norrismi0@gmail.com>
2020-07-15 18:01:29 -0400

    user card

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Dashboard from './components/Dashboard/Dashboard'
-import NotFound from './components/Navigation/NotFound/NotFound'
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard";
+import NotFound from "./components/Navigation/NotFound/NotFound";
+import UserPage from "./components/User/UserPage/UserPage";
 
 function App() {
   return (
     <Router>
-    <Switch>
-      <Route path="/" exact component={Dashboard} />
-      <Route path="*" component={NotFound} />
-    </Switch>
-  </Router>
+      <Switch>
+        <Route path="/MyPage" component={UserPage} />
+        <Route path="/" exact component={Dashboard} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/User/UserPage/UserPage.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import TopNav from "../../Navigation/TopNav/TopNav";
+import "./UserPageStyles.css";
+import { FaShareSquare, FaPen, FaTimes } from "react-icons/fa";
+
+class UserPage extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <TopNav />
+
+        <div className="card-wrapper">
+          <button>
+            <FaShareSquare
+              id="btn-icon"
+              className="btn-share"
+              title="Share idea with community"
+            />
+          </button>
+          <div className="main-card">
+            <div className="text-items">
+              <div className="title">
+                <div className="title-supplement">Supplement:</div>
+                <div className="title-benefit">Benefit:</div>
+                <div className="title-description">Description:</div>
+                <div className="title-date">Date:</div>
+              </div>
+
+              <div className="input">
+                <div className="supplement">Tart Cherry</div>
+                <div className="benefit">Sleep</div>
+                <div className="description">Will help you sleep at night</div>
+                <div className="date">7-15-20</div>
+              </div>
+            </div>
+          </div>
+          <button>
+            <FaPen id="btn-icon" className="edit" title="Edit" />
+          </button>
+          <button>
+            <FaTimes id="btn-icon" className="delete" title="Delete" />
+          </button>
+        </div>
+
+
+      </div>
+    );
+  }
+}
+
+export default UserPage;
